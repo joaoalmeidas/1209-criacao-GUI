e7f8dd4b0a0f71013adde6bfc3105306f3978e5c
joao <joao182cx@gmail.com>
2019-01-17 16:18:00 -0200

    Modificações finais

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,7 +4,6 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 
-
 		CalculadoraFrame frame = new CalculadoraFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(200, 200);
